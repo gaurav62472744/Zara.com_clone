6c7d04159191ed49a3d3596c3b0cb5f192d3b00f
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2022-12-16 14:01:11 +0530

    day-3 added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Cart from "../pages/Cart";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import Product from "../pages/Product";
 import Women from "../pages/Women";
 import Mens from "../pages/Men";
 import Kids from "../pages/Kids";
@@ -11,6 +10,8 @@ import ProductAddToCart from "../pages/WomenCards";
 import MensProductAddToCart from "../pages/MenCards";
 import KidsProductAddToCart from "../pages/KidsCard";
 import PageNotFound from "../pages/PageNotFound";
+import WomenSinglePage from "../pages/WomenSinglePage";
+import Search from "../pages/Search";
 
 const AllRoutes = () => {
   return (
@@ -19,13 +20,14 @@ const AllRoutes = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/Login" element={<Login />}></Route>
-        <Route path="/products" element={<Product />}></Route>
         <Route path="/women" element={<Women />}></Route>
         <Route path="/mens" element={<Mens />}></Route>
         <Route path="/kids" element={<Kids />}></Route>
         <Route path="/womencards" element={<ProductAddToCart />}></Route>
         <Route path="/mencards" element={<MensProductAddToCart />}></Route>
         <Route path="/kidcards" element={<KidsProductAddToCart />}></Route>
+        <Route path="/womenproduct/:id" element={<WomenSinglePage />}></Route>
+        <Route path="/search" element={<Search />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </div>
--- src/pages/WomenCards.jsx
+++ src/pages/WomenCards.jsx
@@ -6,8 +6,8 @@ import {
   chakra,
   Tooltip,
 } from "@chakra-ui/react";
-
-function ProductAddToCart({ imgUrl, prod_name, price }) {
+import { Link } from "react-router-dom";
+function ProductAddToCart({ _id, imgUrl, prod_name, price }) {
   return (
     <Flex
       p={50}
@@ -17,60 +17,65 @@ function ProductAddToCart({ imgUrl, prod_name, price }) {
       justifyContent="center"
       ml={"5px"}
     >
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        maxW="sm"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-      >
-        <Image
-          src={imgUrl}
-          w={"400px"}
-          h={"600px"}
-          alt={`Picture of ${prod_name}`}
-          roundedTop="lg"
-        />
+      <Link to={`/womenproduct/${_id}`}>
+        <Box
+          bg={useColorModeValue("white", "gray.800")}
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          shadow="lg"
+          position="relative"
+        >
+          <Image
+            src={imgUrl}
+            w={"400px"}
+            h={"600px"}
+            alt={`Picture of ${prod_name}`}
+            roundedTop="lg"
+          />
 
-        <Box p="6">
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-              fontSize="sm"
-              fontFamily={"Neue-Helvetica, Helvetica, Arial, sans-serif"}
-            >
-              {prod_name}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1.2em"}
-            >
-              <chakra.a href={"#"} display={"flex"}></chakra.a>
-            </Tooltip>
-          </Flex>
-
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="2xl" color={useColorModeValue("gray.800", "white")}>
+          <Box p="6">
+            <Flex mt="1" justifyContent="space-between" alignContent="center">
               <Box
-                as="span"
-                color={"gray.600"}
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
                 fontSize="sm"
                 fontFamily={"Neue-Helvetica, Helvetica, Arial, sans-serif"}
               >
-                $
+                {prod_name}
               </Box>
-              {price}
-            </Box>
-          </Flex>
+              <Tooltip
+                label="Add to cart"
+                bg="white"
+                placement={"top"}
+                color={"gray.800"}
+                fontSize={"1.2em"}
+              >
+                <chakra.a href={"#"} display={"flex"}></chakra.a>
+              </Tooltip>
+            </Flex>
+
+            <Flex justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="2xl"
+                color={useColorModeValue("gray.800", "white")}
+              >
+                <Box
+                  as="span"
+                  color={"gray.600"}
+                  fontSize="sm"
+                  fontFamily={"Neue-Helvetica, Helvetica, Arial, sans-serif"}
+                >
+                  $
+                </Box>
+                {price}
+              </Box>
+            </Flex>
+          </Box>
         </Box>
-      </Box>
+      </Link>
     </Flex>
   );
 }
