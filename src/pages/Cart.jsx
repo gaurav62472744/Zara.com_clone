6bf424b8a76de1e5607e858617bdc72d4df558a7
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2022-12-18 02:09:53 +0530

    added to day-5

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,18 @@
 import { Box, Flex, Text, Image, Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 function Cart() {
   let lData = JSON.parse(localStorage.getItem("Data"));
 
   console.log(lData);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    let newData = lData.splice(id, 1);
+    localStorage.setItem("Data", JSON.stringify(newData));
+  };
+
+  const [incr, setIncr] = useState(1);
 
   return (
     <Box style={{ width: "98vw", margin: "auto" }}>
@@ -18,7 +27,7 @@ function Cart() {
         </Flex>
 
         {lData.map((el, i) => (
-          <Box key={i} style={{ width: "50%", margin: "auto" }}>
+          <Box key={i} style={{ width: "75%", margin: "auto" }}>
             <Flex
               border={"3px solid black"}
               marginTop={"20px"}
@@ -26,24 +35,44 @@ function Cart() {
               fontSize={"20px"}
               alignItems={"center"}
               fontFamily={"Neue-Helvetica, Helvetica, Arial, sans-serif"}
-              padding={"8px"}
+              padding={"1rem"}
               mb={"10px"}
             >
               <Image
-                w={"130px"}
+                w={"200px"}
                 src={el.imgUrl}
                 borderRadius={"20px"}
                 alt={el.prod_name}
               ></Image>
-              <Text>Price : $ {el.price}</Text>
+              <Text>Price : $ {el.price * incr}</Text>
               <Text>Size : {el.size}</Text>
               <Text>Category : {el.type1}</Text>
               <Button
                 bg={"black"}
                 color={"white"}
                 _hover={{ bg: "grey", color: "white" }}
+                onClick={() => handleDelete(el.id)}
               >
                 Delete
+              </Button>
+
+              <Button
+                bg={"black"}
+                color={"white"}
+                _hover={{ bg: "grey", color: "white" }}
+                onClick={() => setIncr(incr + 1)}
+              >
+                +
+              </Button>
+              <Text>{incr}</Text>
+              <Button
+                bg={"black"}
+                color={"white"}
+                _hover={{ bg: "grey", color: "white" }}
+                onClick={() => setIncr(incr - 1)}
+                disabled={incr === 1}
+              >
+                -
               </Button>
             </Flex>
           </Box>
